c8eab7bc8373e7fba5900b0cac5589e5b20ec10d
TAHOMI RASIKH <S6105692@live.tees.ac.uk>
2020-08-22 18:01:13 +0100

    Finished Login and Sign up

--- app/src/main/java/com/example/EcommerceShop/LoginActivity.java
+++ app/src/main/java/com/example/EcommerceShop/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.example.EcommerceShop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.EcommerceShop.Model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity
 {
     private Button LoginButton;
     private EditText InputPhone, InputPassword;
+    private ProgressDialog loadingBar;
+    private String parentdatabaseName = "Users";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -19,5 +34,83 @@ public class LoginActivity extends AppCompatActivity
         LoginButton = (Button) findViewById(R.id.login_button);
         InputPhone = (EditText) findViewById(R.id.login_phone_number);
         InputPassword = (EditText) findViewById(R.id.login_password);
+        loadingBar = new ProgressDialog(this);
+
+
+        LoginButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                LoginUser();
+            }
+        });
     }
+
+    private void LoginUser()
+    {
+        String phone = InputPhone.getText().toString();
+        String password = InputPassword.getText().toString();
+        if(TextUtils.isEmpty(phone))
+        {
+            Toast.makeText(this, "Please Write Your Phone Number", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please Write Your Password", Toast.LENGTH_SHORT).show();
+        }
+        else
+            {
+                loadingBar.setTitle("Login Account");
+                loadingBar.setMessage("Please Wait, while we get your details");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+
+                AllowAccessToAccount(phone, password);
+        }
+    }
+
+    private void AllowAccessToAccount(final String phone, final String password)
+    {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if(dataSnapshot.child(parentdatabaseName).child(phone).exists())
+                {
+                    Users userData = dataSnapshot.child(parentdatabaseName).child(phone).getValue(Users.class);
+
+                    if(userData.getPhone().equals(phone))
+                    {
+                        if(userData.getPhone().equals(password))
+                        {
+                            Toast.makeText(LoginActivity.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                }
+                else 
+                    {
+                        Toast.makeText(LoginActivity.this, "This" + phone + "doesn't exist", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                        Toast.makeText(LoginActivity.this, "You need to create this account", Toast.LENGTH_SHORT).show();
+                    }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/EcommerceShop/RepeatedInfo/ForgottenPassword.java
@@ -0,0 +1,8 @@
+package com.example.EcommerceShop.RepeatedInfo;
+
+import com.example.EcommerceShop.Model.Users;
+
+public class ForgottenPassword
+{
+    private static Users LoggedinUsers;
+}
