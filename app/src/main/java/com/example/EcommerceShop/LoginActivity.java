f1da801a67cd15111695ac5a85bc4b61c8405132
TAHOMI RASIKH <S6105692@live.tees.ac.uk>
2020-08-22 16:52:53 +0100

    Added Firebase with validation

--- app/src/main/java/com/example/EcommerceShop/LoginActivity.java
+++ app/src/main/java/com/example/EcommerceShop/LoginActivity.java
@@ -3,12 +3,21 @@ package com.example.EcommerceShop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity
+{
+    private Button LoginButton;
+    private EditText InputPhone, InputPassword;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginButton = (Button) findViewById(R.id.login_button);
+        InputPhone = (EditText) findViewById(R.id.login_phone_number);
+        InputPassword = (EditText) findViewById(R.id.login_password);
     }
 }
--- app/src/main/java/com/example/EcommerceShop/RegisterActivity.java
+++ app/src/main/java/com/example/EcommerceShop/RegisterActivity.java
@@ -1,15 +1,33 @@
 package com.example.EcommerceShop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity
 {
     private Button CreateAccountButton;
     private EditText InputName, InputPhoneNumber, InputPassword;
+    private ProgressDialog loadingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +37,95 @@ public class RegisterActivity extends AppCompatActivity
         InputName = (EditText) findViewById(R.id.register_username);
         InputPhoneNumber = (EditText) findViewById(R.id.register_phone_number);
         InputPassword = (EditText) findViewById(R.id.register_password);
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                CreateAccount();
+            }
+        });
+    }
+
+
+    private void CreateAccount()
+    {
+        String name = InputName.getText().toString();
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+        loadingBar = new ProgressDialog(this);
+
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this, "Please Write Your Name", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "Please Write Your Phone Number", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please Write Your Password", Toast.LENGTH_SHORT).show();
+        }
+        else
+            {
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please Wait, while we get your details");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            Validatephonenumber(name, phone, password);
+        }
+    }
+
+    private void Validatephonenumber(final String name, final String phone, final String password)
+    {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if(!(dataSnapshot.child("name").child(phone).exists()))
+                {
+                    HashMap<String, Object> userinfoMap = new HashMap<>();
+                    userinfoMap.put("phone", phone);
+                    userinfoMap.put("password", password);
+                    userinfoMap.put("name", name);
+
+                    RootRef.child("Users").child(phone).updateChildren(userinfoMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>()
+                            {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(RegisterActivity.this, "Congrats you are registered", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    else{
+                                        loadingBar.dismiss();
+                                        Toast.makeText(RegisterActivity.this, "Error Unable to Update Try Again", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this, "This " +  phone + "Exists" , Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Please Try Again Using Another Phone Number", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+            }
+        });
     }
 }
